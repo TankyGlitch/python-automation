8cf089146c94708ea25d16a8a94c368815600575
TankyGlitch <anantctiwary@gmail.com>
2026-09-05 20:56:03 +0530

    Made changes

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-print(os.getcwd())
-print(os.listdir())
-x = os.listdir()
-print(type(x))
---
+++ osmodule.py
@@ -0,0 +1,8 @@
+import os
+#for getting the current working directory.
+print(os.getcwd())
+#for listing the files present in the current directory
+print(os.listdir())
+x = os.listdir()
+#return type of listdir is list.
+print(type(x))

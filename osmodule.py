ee167d67c1007e7759a121c7541abb95a6bd69f1
TankyGlitch <anantctiwary@gmail.com>
2026-09-05 21:22:25 +0530

    Ma

--- osmodule.py
+++ osmodule.py
@@ -6,3 +6,19 @@ print(os.listdir())
 x = os.listdir()
 #return type of listdir is list.
 print(type(x))
+#os.listdir() shows both files AND directories (folders).
+for files in os.listdir():
+
+    print("The names are", files)
+#isfile() - to check if the given is a file or not
+#isdir()  - to check if the given is a directory or not.
+#using isdir, isfil returns true or false values. if we want to use in for loop we need to give path and check.
+for x in os.listdir():
+    if os.path.isfile(x):
+        #os.path.isfile(filename)
+        print("The file name is: ", x)
+    elif os.path.isdir(x):
+        #os.path.isdir(dirname)
+        print("The dir name is: ", x)
+#if we want to check if something even exists as a whole
+
